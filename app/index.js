ff6fed3b9e771011d507b1223f2326423e88a2cf
Zoltan Tuskes <tuskesz@gmail.com>
2021-07-21 23:38:31 +0200

    review fixes

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import document from "document";
 
 import * as clock from "./clock";
 import * as newfile from "./newfile";
-import { tempCFConv } from "../common/utils";
+import { toFahrenheit } from "../common/utils";
+import { units } from "user-settings";
 
 const time = document.getElementById("time");
 const details = document.getElementById("details");
@@ -15,9 +16,9 @@ clock.initialize("minutes", data => {
 newfile.initialize(data => {
   // fresh weather file received
 
-  // if user-settings temperature == F and the data.unit == Celsius then we convert to Fahrenheit
-  // This use only if the getWeatherData() function we use without optional parameter.
-  data = units.temperature == "F" ? tempCFConv(data): data;
+  // If the user-settings temperature == F and the result data.unit == Celsius then we convert to Fahrenheit
+  // Use only if the getWeatherData() function you use without optional parameter.
+  data = units.temperature === "F" ? toFahrenheit(data): data;
 
   details.text = `It's ${data.temperature}\u00B0 ${data.unit} and ${data.condition} in ${data.location}`;
   clock.tick();
--- common/utils.js
+++ common/utils.js
@@ -13,9 +13,9 @@ export function zeroPad(i) {
 * Convert Celsius to Fahrenheit
 * @param {object} data - WeatherData -
 */
-export function tempCFConv(data) {
+export function toFahrenheit(data) {
   
-  if (data.unit.toLowerCase() == "celsius") {
+  if (data.unit.toLowerCase() === "celsius") {
      data.temperature =  Math.round((data.temperature * 1.8)+32);
      data.unit = "Fahrenheit";
   }
